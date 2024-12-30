7588af6e3e6308dc1ea02ff3a27dfacad0d704f8
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-12-30 12:17:23 +0530

    types

---
+++ build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let myName;
+myName = 'Nitesh';
+myName = 'Kumar';
+const sum = (a, b) => {
+    return a + b;
+};
+console.log(myName);
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+let myName: string
+myName = 'Nitesh'
+myName = 'Kumar'
+
+const sum = (a: number, b: number) =>{
+    return a + b
+}
+
+console.log(myName)
