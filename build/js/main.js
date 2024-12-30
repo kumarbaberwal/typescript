76feb5faeffbf8cc8598c9fe8d6768d900e29623
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-12-30 23:00:59 +0530

    functions

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,25 @@
 "use strict";
-let myName;
-myName = 'Nitesh';
-myName = 'Kumar';
+const student = {
+    name: 'kumar',
+    rollno: 106,
+    class: 'B. Tech CSE'
+};
+const student1 = {
+    name: 'kumar',
+    rollno: 106,
+    class: 'B. Tech CSE',
+    subjects: ['Maths'],
+};
 const sum = (a, b) => {
     return a + b;
 };
-console.log(myName);
+const sub = function (a, b) {
+    return a - b;
+};
+console.log(sum(2, 3));
+const logMsg = (msg) => {
+    console.log(`Message: ${msg}`);
+};
+logMsg('Kumar');
+console.log(student.subjects);
+console.log(student1);
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,45 @@
-let myName: string
-myName = 'Nitesh'
-myName = 'Kumar'
+type school = {
+    name: string,
+    rollno: number,
+    class: string,
+    subjects?: (string | number) []
+}
 
-const sum = (a: number, b: number) =>{
+const student: school = {
+    name: 'kumar',
+    rollno: 106,
+    class: 'B. Tech CSE'
+}
+
+interface college {
+    name: string,
+    rollno: number,
+    class: string,
+    subjects: [string | number]
+}
+
+const student1: college = {
+    name: 'kumar',
+    rollno: 106,
+    class: 'B. Tech CSE',
+    subjects: ['Maths'],
+}
+
+const sum = (a: number, b: number): number => {
     return a + b
 }
 
-console.log(myName)
+const sub = function (a: number, b: number): number {
+    return a - b
+}
+
+console.log(sum(2, 3))
+
+const logMsg = (msg: string): void => {
+    console.log(`Message: ${msg}`)
+}
+
+logMsg('Kumar')
+
+console.log(student.subjects)
+console.log(student1)
